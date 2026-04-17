334e78df482c00445d1f4cf67160b8bc82cfe136
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-04-17 17:48:18 -0600

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        stk = []
+        for i, t in enumerate(temperatures):
+            while stk and stk[-1][0] < t:
+                stk_t, stk_i = stk.pop()
+                res[stk_i] = i - stk_i
+            stk.append((t,i))
+        return res
+        
