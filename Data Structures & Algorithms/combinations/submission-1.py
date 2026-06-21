bc405a3fbf585ddbbac6d2a3d220c0fff24d7816
ANURAG VEMULA <101939604+Smokeengine@users.noreply.github.com>
2026-06-21 01:40:44 -0600

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        arr = []
+        for i in range(n):
+            arr.append(i+1)
+        res = []
+        def helper(i, cur):
+            if len(cur) == k:
+                res.append(list(cur))
+                return
+            if n-i < k - len(cur):
+                return
+            
+            cur.append(arr[i])
+            helper(i+1, cur)
+
+            cur.pop()
+            helper(i+1, cur)
+        helper(0, [])
+        return res
+            
+
+
+        
